488bcfb1fd041de1b4692795a594060ca61d5d89
gjiwierzbicki <gjiwierzbicki@gmail.com>
2024-02-27 16:22:59 -0500

    Readded "EXIT" button and made it redirect to about:blank

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("penis");
+console.log("penis")
     
-    // const exit_button = document.getElementById("exit")
+    const exit_button = document.getElementById("exit")
     const play_button = document.getElementById("play")
 
+    // Can only redirect to blank page.
+    exit_button.addEventListener("click", function (event) {
+        if (event.target === exit_button) {
+            new_window = window.open("about:blank", "_self");
+        }
+    });
+
+    // ToDo
     play_button.addEventListener("click", function (event) {
         if (event.target === play_button) {
             console.log("To do");
