d4370218ad71447e61a4a6eb47d64b7ff0c360d1
gjiwierzbicki <gjiwierzbicki@gmail.com>
2024-03-01 04:40:54 -0500

    Added Flask-Pymongo and made the database

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
+
+# Connecting MongoDB
+app.config["MONGO_URI"] = "mongodb://localhost:27017/BlackJack"
+mongo = PyMongo(app)
+
+# Making Tables
+user_table = mongo.db.user
+password_table = mongo.db.password
+chat_table = mongo.db.table
+
+
 
 # index page
 @app.route('/')
