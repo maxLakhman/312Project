33418ae974273af1a9cf2f342f55a83c6d318d82
gjiwierzbicki <gjiwierzbicki@gmail.com>
2024-03-14 16:22:14 -0400

    Added auth tokens for messages

--- routes/auth.py
+++ routes/auth.py
@@ -1,28 +1,29 @@
-import json, bcrypt
-from flask import Flask, render_template, request, jsonify, Blueprint
+import json, bcrypt, secrets, hashlib
+from flask import Flask, render_template, request, jsonify, Blueprint, make_response
 from pymongo import MongoClient
 
-auth_blueprint = Blueprint('auth_blueprint', __name__, template_folder='templates')
+auth_blueprint = Blueprint("auth_blueprint", __name__, template_folder="templates")
 
 # Connecting to MongoDB
-mongo_client = MongoClient('db')
+mongo_client = MongoClient("db")
 db = mongo_client["BlackJack"]
 
 # Making collections
 user_collection = db["user"]
 
+
 # auth routing framework
-@auth_blueprint.route('/auth', methods=['POST'])
+@auth_blueprint.route("/auth", methods=["POST"])
 def auth():
     # Getting JSON request and filling variables
     received_data = request.get_json()
-    username = received_data.get('username')
-    password = received_data.get('password')
+    username = received_data.get("username")
+    password = received_data.get("password")
 
     # HTML escape characters
-    username = username.replace('&', '&amp;')
-    username = username.replace('<', '&lt;')
-    username = username.replace('>', '&gt;')
+    username = username.replace("&", "&amp;")
+    username = username.replace("<", "&lt;")
+    username = username.replace(">", "&gt;")
 
     # Getting record of username
     user_cursor = user_collection.find({"username": username})
@@ -35,7 +36,7 @@ def auth():
     elif not user_list:
         response_data = {"status": "error", "message": "Invalid Credentials"}
     # Adding salt to given pw, hashing, then comparing to known user hash
-    else: 
+    else:
         user_password = user_list[0]["hash"]
         user_salt = user_list[0]["salt"]
         password = password.encode()
@@ -43,32 +44,41 @@ def auth():
         check_pass = bcrypt.hashpw(password, user_salt)
 
         if check_pass == user_password:
+            # Making auth token log2(64^256) = 1536 bits of entropy
+            auth_token = secrets.token_urlsafe(256)
+            token_hash = hashlib.sha256(auth_token.encode()).hexdigest()
+            user_collection.update_one(
+                {"username": username}, {"$set": {"auth_token": token_hash}}
+            )
+
+            # Making response
             response_data = {"status": "success", "message": "Welcome " + username}
+            response = make_response(jsonify(response_data))
+            response.set_cookie("auth_token", auth_token)
+            return response
         else:
             response_data = {"status": "error", "message": "Invalid Credentials"}
-
 
     return jsonify(response_data)
 
 
-
-@auth_blueprint.route('/register', methods=['POST'])
+@auth_blueprint.route("/register", methods=["POST"])
 def register():
     # Getting JSON request and filling variables
     received_data = request.get_json()
-    username = received_data.get('username')
+    username = received_data.get("username")
     password = received_data.get("password")
     password_confirm = received_data.get("password_confirm")
 
     # HTML escape characters
-    username = username.replace('&', '&amp;')
-    username = username.replace('<', '&lt;')
-    username = username.replace('>', '&gt;')
+    username = username.replace("&", "&amp;")
+    username = username.replace("<", "&lt;")
+    username = username.replace(">", "&gt;")
 
     # Checking if there will be duplicate username
     user_cursor = user_collection.find({"username": username})
     user_list = list(user_cursor)
-    
+
     # Empty Field
     if not username or not password or not password_confirm:
         response_data = {"status": "error", "message": "Empty Field"}
@@ -80,19 +90,23 @@ def register():
     # Password Missmatch
     elif password != password_confirm:
         response_data = {"status": "error", "message": "Password Mismatch"}
-    
+
     # Register User
     else:
         # Hashing & salting password
         salt = bcrypt.gensalt()
         password = password.encode()
         password_hash = bcrypt.hashpw(password, salt)
-        
+
         # Making record and adding to database
-        record = {"username": username, "hash": password_hash, "salt": salt}
+        record = {
+            "username": username,
+            "hash": password_hash,
+            "salt": salt,
+            "tokens": 500,
+        }
         user_collection.insert_one(record)
 
         response_data = {"status": "success", "message": "Registration Successful"}
-       
 
     return jsonify(response_data)
--- routes/chat.py
+++ routes/chat.py
@@ -1,41 +1,58 @@
-import json, bcrypt
+import json, bcrypt, hashlib
 from flask import Flask, render_template, request, jsonify, Blueprint
 from pymongo import MongoClient
-from bson.json_util import dumps, loads 
+from bson.json_util import dumps, loads
+from routes.auth import user_collection
 
 # Connecting to MongoDB
-mongo_client = MongoClient('db')
+mongo_client = MongoClient("db")
 db = mongo_client["BlackJack"]
 
 # Making collections
 chat_collection = db["chat"]
 
-chat_blueprint = Blueprint('chat_blueprint', __name__, template_folder='templates')
-@chat_blueprint.route('/chat-messages', methods=['POST'])
+chat_blueprint = Blueprint("chat_blueprint", __name__, template_folder="templates")
+
+
+@chat_blueprint.route("/chat-messages", methods=["POST"])
 def chatPost():
     received_data = request.get_json()
+
+    # Checking auth token
+    if "auth_token" in request.cookies:
+        browser_token = request.cookies.get("auth_token")
+        hashed_browser_token = hashlib.sha256(browser_token.encode()).hexdigest()
+
+        # Finding user with same auth token
+        user = user_collection.find_one(
+            {"auth_token": hashed_browser_token}, {"_id": 0}
+        )
+        if user:
+            received_data["username"] = user.get("username")
+
     print(f"recieved data: {received_data}")
     chat_collection.insert_one(received_data)
 
     print(chat_collection.find({}))
 
-    list_cur = list(chat_collection.find({})) 
-  
-    # Converting to the JSON 
-    json_data = dumps(list_cur, indent = 2)  
+    list_cur = list(chat_collection.find({}))
+
+    # Converting to the JSON
+    json_data = dumps(list_cur, indent=2)
     print(json_data)
 
     return jsonify(json_data)
 
-@chat_blueprint.route('/chat-messages', methods=['GET'])
+
+@chat_blueprint.route("/chat-messages", methods=["GET"])
 def chat_get():
-    chat_box = request.args.get('chat_box')
+    chat_box = request.args.get("chat_box")
     print(f"received data: {chat_box}")
 
-    list_cur = list(chat_collection.find({"chat_box": chat_box})) 
-  
-    # Converting to the JSON 
-    json_data = dumps(list_cur, indent=2)  
+    list_cur = list(chat_collection.find({"chat_box": chat_box}))
+
+    # Converting to the JSON
+    json_data = dumps(list_cur, indent=2)
     print(json_data)
 
     return jsonify(json_data)
