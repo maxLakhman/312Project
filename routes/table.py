068cc728b7191b031dab024f5457f24da9ef79ff
gjiwierzbicki <gjiwierzbicki@gmail.com>
2024-04-25 22:11:31 -0400

    Fixed issue where table creator doesn't start with cards & can't hit.

--- routes/table.py
+++ routes/table.py
@@ -51,7 +51,7 @@ def create_table():
     # create a new table
     table = {
         "table_id": str(table_collection.count_documents({}) + 1),
-        "players": [username],
+        "players": [],
         "deck": ["2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
                  "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
                  "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
